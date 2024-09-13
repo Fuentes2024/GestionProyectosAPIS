2911ed42370da6347d47e2853ade09645437b4f6
Isaiaslue42637 <Isaiaslue42637@outlook.com>
2024-09-13 08:53:03 -0600

    tarea y miembro

--- GestionProyectosAPI/Models/MiembroEquipo.cs
+++ GestionProyectosAPI/Models/MiembroEquipo.cs
@@ -8,25 +8,14 @@ namespace GestionProyectosAPI.Models;
 
 public partial class MiembroEquipo
 {
-    [Key]
-    [Column("miembroEquipoId")]
+  
     public int MiembroEquipoId { get; set; }
-
-    [Column("cargo")]
-    [StringLength(50)]
-    [Unicode(false)]
     public string? Cargo { get; set; }
 
-    [Column("usuarioId")]
     public int UsuarioId { get; set; }
-
-    [InverseProperty("MiembroEquipo")]
     public virtual ICollection<Informe> Informe { get; set; } = new List<Informe>();
 
-    [InverseProperty("MiembroEquipo")]
     public virtual ICollection<Tarea> Tarea { get; set; } = new List<Tarea>();
 
-    [ForeignKey("UsuarioId")]
-    [InverseProperty("MiembroEquipo")]
     public virtual Usuario Usuario { get; set; } = null!;
 }
