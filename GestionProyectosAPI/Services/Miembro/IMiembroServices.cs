1b8e2046bb141a39d0222264bb7ce80c6014e4de
Isaiaslue42637 <Isaiaslue42637@outlook.com>
2024-09-16 08:17:37 -0600

    rol y miembro

--- GestionProyectosAPI/Services/Miembro/IMiembroServices.cs
+++ GestionProyectosAPI/Services/Miembro/IMiembroServices.cs
@@ -1,6 +1,13 @@
-﻿namespace GestionProyectosAPI.Services.Miembro
+﻿using GestionProyectosAPI.DTOs;
+
+namespace GestionProyectosAPI.Services.Miembro
 {
     public interface IMiembroServices
     {
+        Task<int> PostMiembro(MiembroEquipoRequest miembroEquipo);
+        Task<List<MiembroEquipoResponse>> GetMiembros();
+        Task<MiembroEquipoResponse> GetMiembro(int miembroEquipoId);
+        Task<int> PutMiembro(int miembroEquipoId, MiembroEquipoRequest miembroEquipo);
+        Task<int> DeleteMiembro(int miembroEquipoId);
     }
 }
---
+++ GestionProyectosAPI/Services/Rol/IRol.cs
@@ -0,0 +1,13 @@
+﻿using GestionProyectosAPI.DTOs;
+
+namespace GestionProyectosAPI.Services.Rol
+{
+    public interface IRol
+    {
+        Task<int> PostRol(RolRequest rol);
+        Task<List<RolResponse>> GetRols();
+        Task<RolResponse> GetRol(int rolId);
+        Task<int> PutRol(int rolId, RolRequest rol);
+        Task<int> DeleteRol(int rolId);
+    }
+}
